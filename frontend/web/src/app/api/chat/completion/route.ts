20e590a9a858aa6aea64ccb6724487cf3ca0e6f6
eunsongkoh <eunsongkohh@gmail.com>
2024-09-15 02:20:01 -0400

    Updated imports

--- frontend/web/src/app/api/chat/completion/route.ts
+++ frontend/web/src/app/api/chat/completion/route.ts
@@ -1,16 +1,11 @@
-import { CohereStream } from 'ai'
-import { COHERE_API_KEY } from './secret.ts';
+import { CohereStream } from "ai";
+import { COHERE_API_KEY } from "./secret";
 
-// IMPORTANT! Set the runtime to edge
-
-const API_KEY = COHERE_API_KEY
+const API_KEY = COHERE_API_KEY;
 
 export async function POST(req: Request) {
-  console.log(req)
-  // Extract the `prompt` from the body of the request
   const { prompt } = await req.json();
-  console.log(prompt)
-  // console.log(process.env.COHERE_API_KEY)
+  console.log(prompt);
 
   const data = `
     OrderDate	Region	Manager	SalesMan	Item	Units	 Unit_price 	Sale_amt
@@ -65,26 +60,26 @@ export async function POST(req: Request) {
 
   const body = JSON.stringify({
     prompt: prompt_data,
-    model: 'command-nightly',
+    model: "command-nightly",
     max_tokens: 300,
     stop_sequences: [],
     temperature: 0.9,
-    return_likelihoods: 'NONE',
-    stream: true
+    return_likelihoods: "NONE",
+    stream: true,
   });
 
-  const response = await fetch('https://api.cohere.ai/v1/generate', {
-    method: 'POST',
+  const response = await fetch("https://api.cohere.ai/v1/generate", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${API_KEY}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
     },
-    body
+    body,
   });
 
   // Extract the text response from the Cohere stream
-  const stream = CohereStream(response)
+  const stream = CohereStream(response);
 
   // Respond with the stream
-  return new Response(stream)
+  return new Response(stream);
 }
