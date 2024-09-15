f3dd3c90e3b5080a10e8d8671f432b0bc33c4d57
eunsongkoh <eunsongkohh@gmail.com>
2024-09-14 23:55:41 -0400

    added video stabilization

--- filters.py
+++ filters.py
@@ -1,48 +1,68 @@
 import cv2
 import os
 import shutil
+from vidstab import VidStab
 import numpy as np
+
 
 def filter_images(image_file_list, filter_list):
     for output_option in filter_list:
-        if os.path.exists(f'output/{output_option}'):
-            shutil.rmtree(f'output/{output_option}')
-            
-        os.mkdir(f'output/{output_option}')
-        
+        if os.path.exists(f"output/{output_option}"):
+            shutil.rmtree(f"output/{output_option}")
+
+        os.mkdir(f"output/{output_option}")
+
         for file in image_file_list:
             image = cv2.imread(file)
             result = FUNCTION_NAME_TO_FUNCTION[output_option.lower()](image)
-            cv2.imwrite(f'output/{output_option}/{file[file.rindex('/'):]}', result)
+            cv2.imwrite(f"output/{output_option}/{file[file.rindex('/'):]}", result)
+
 
 def filter_videos(video_file_list, filter_list):
-    pass
+    stabilizer = VidStab()
+
+    for output_option in filter_list:
+        if os.path.exists(f"output/{output_option}"):
+            shutil.rmtree(f"output/{output_option}")
+
+        os.mkdir(f"output/{output_option}")
+
+        for file in video_file_list:
+            image = stabilizer.stabilize(
+                file, f"output/{output_option}/{file[file.rindex('/'):]}"
+            )
+            print(file)
+
 
 def box_blur(image):
     (height, width, _) = image.shape
-    
+
     n = min(height, width) // 30
-    kernel = 1/(n*n) * np.array([[1 for _ in range(n)] for _ in range(n)])
-    
+    kernel = 1 / (n * n) * np.array([[1 for _ in range(n)] for _ in range(n)])
+
     value = cv2.filter2D(image, -1, kernel)
     return value
 
+
 def noise_reduction_colour(image):
-    return cv2.fastNlMeansDenoisingColored(image,None,10,10,7,21)
+    return cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
+
 
 def noise_reduction_bw(image):
-    return cv2.fastNlMeansDenoising(image,None,10,7,21)
+    return cv2.fastNlMeansDenoising(image, None, 10, 7, 21)
+
 
 def to_grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+
 def canny_edges(img):
     img = to_grayscale(img)
-    return cv2.Canny(img,100,200)
+    return cv2.Canny(img, 100, 200)
+
 
 FUNCTION_NAME_TO_FUNCTION = {
-    'blur': box_blur,
-    'grayscale': to_grayscale,
-    'canny': canny_edges, 
-    'gaussian blur': box_blur
+    "blur": box_blur,
+    "grayscale": to_grayscale,
+    "canny edges": canny_edges,
 }
--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import json
 
 from filters import filter_images, filter_videos
 
-base_download_path = 'portal-files/Portal Downloads/Hack the North'
+base_download_path = "input/"
 package_name = os.listdir(base_download_path)[0]
 
 # should be our only directory
@@ -15,15 +15,32 @@ if not os.path.isdir(full_path):
 else:
     # get all files, image files, and the specific json file
     all_files = os.listdir(full_path)
-    json_file = [f for f in os.listdir(full_path) if f[-4:] == 'json']
-    image_files = [f'{full_path}/{f}' for f in os.listdir(full_path) if f.lower().endswith('jpg') or f.lower().endswith('jpeg') or f.lower().endswith('png')]
-    video_files = [f'{full_path}/{f}' for f in os.listdir(full_path) if f.lower().endswith('mp4')]
-    
-    with open(f'{full_path}/{json_file[0]}') as file:
+    json_file = [f for f in os.listdir(full_path) if f[-4:] == "json"]
+    image_files = [
+        f"{full_path}/{f}"
+        for f in os.listdir(full_path)
+        if f.lower().endswith("jpg")
+        or f.lower().endswith("jpeg")
+        or f.lower().endswith("png")
+    ]
+
+    video_files = [
+        f"{full_path}/{f}" for f in os.listdir(full_path) if f.lower().endswith("mp4")
+    ]
+    excel_files = [
+        f"{full_path}/{f}"
+        for f in os.listdir(full_path)
+        if f.lower().endswith("xlsx") or f.lower().endswith("xls")
+    ]
+    csv_files = [
+        f"{full_path}/{f}" for f in os.listdir(full_path) if f.lower().endswith("csv")
+    ]
+
+    with open(f"{full_path}/{json_file[0]}") as file:
         metadata = json.load(file)
-        
-    image_options = metadata['image_options'].split(',')
-    video_options = metadata['video_options'].split(',')
-    
+
+    image_options = metadata["image_options"].split(",")
+    video_options = metadata["video_options"].split(",")
+
     filter_images(image_files, image_options)
-    filter_videos(video_files, video_options)    
+    filter_videos(video_files, video_options)
