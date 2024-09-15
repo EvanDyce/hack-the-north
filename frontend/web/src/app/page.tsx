dfda23fa539e34effc5f481f11d8dc4376f4b895
eunsongkoh <eunsongkohh@gmail.com>
2024-09-15 02:49:33 -0400

    made front end updates

--- frontend/web/src/app/page.tsx
+++ frontend/web/src/app/page.tsx
@@ -2,12 +2,12 @@
 import React, { useState } from "react";
 import Completion from "./components/completion";
 import Logo from "./components/logo";
-import './globals.css';
+import "./globals.css";
 
 const gradientStyle = {
-  background: 'linear-gradient(-45deg, #7d97f9, #90dcc2, #7d97f9, #ff9ad5)',
-  backgroundSize: '400% 400%',
-  animation: 'gradient 15s ease infinite',
+  background: "linear-gradient(-45deg, #7d97f9, #90dcc2, #7d97f9, #ff9ad5)",
+  backgroundSize: "400% 400%",
+  animation: "gradient 15s ease infinite",
 };
 
 const keyframes = `
@@ -20,18 +20,27 @@ const keyframes = `
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen flex-col items-center justify-center" style={gradientStyle}>
+    <main
+      className="flex min-h-screen min-w-screen flex-col items-center justify-center"
+      style={gradientStyle}
+    >
       <style>{keyframes}</style>
       <Logo />
-      <Completion />
-      <iframe
-        src="https://ssse.portal.massive.io/embed?email=&hidename=false&hidemessage=false"
-        width="375"
-        height="800"
-        className="my-5"
-        scrolling="no"
-        title="Uploader"
-      ></iframe>
+      <div className="bg-white p-3 mb-5 rounded-md flex-col items-center justify-center">
+        <h1 className="text-2xl text-center">
+          Eliminate the mess and streamline your data processing pipeline with
+          one quick upload{" "}
+        </h1>
+        <iframe
+          src="https://ssse.portal.massive.io/embed?email=&hidename=false&hidemessage=false"
+          width="1000"
+          height="800"
+          className="rounded-md"
+          scrolling="no"
+          title="Uploader"
+        ></iframe>
+        <Completion />
+      </div>
     </main>
   );
 }
